a59f8435561d89a1bcba154d4791e15f869913db
Сергей Субботин <zarodiny@yandex.ru>
2022-03-29 15:01:36 +1000

    JavaScript не ломается при удалении блоков

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -4,11 +4,15 @@ import IMask from 'imask';
 
 
 // маска телефона
-let element = document.getElementById('user-phone');
 let maskOptions = {
   mask: '+{7}(000)000-00-00',
 };
-let mask1 = IMask(element, maskOptions);
 
 let element2 = document.getElementById('phone');
 let mask2 = IMask(element2, maskOptions);
+
+const popup = document.querySelector('.popup');
+if (popup) {
+  let element = document.getElementById('user-phone');
+  let mask1 = IMask(element, maskOptions);
+}
